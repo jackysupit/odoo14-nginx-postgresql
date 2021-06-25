c859d1fa2831ac698359d83fc2bb9e98581be137
jacky supit <jsupit@gmail.com>
2021-06-25 12:53:54 +0700

    odoo14 works!

---
+++ data/app/custom_addons/kjd_installer/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'KJD Installer',
+    'summary': 'Ini khusus untuk menginstall semua module, supaya enggak harus install manual satu per satu',
+    'version': '1.001',
+    'category': 'Keluarga JD',
+    'author':'jacky',
+    'maintainer': 'jacky',
+    'website': 'http://www.keluarga.jdteknologi.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        # 'kiseli',
+        # 'kiseli_dev',
+        # 'kiseli_dev_2',
+        # 'kiseli_update_vit_uudp',
+        # 'kiseli_domain_analytic_tags',
+    ],
+    'data': [
+    ],
+    'application': True,
+    'installable': True
+}
